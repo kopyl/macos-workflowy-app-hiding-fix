5a0474d49d46358284580da7c95693c3272a3b82
kopyl <oleggggg1997@gmail.com>
2025-04-17 07:10:06 +0300

    Initial Commit

---
+++ hide-workflowy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hide-workflowy
+//
+//  Created by Oleh Kopyl on 17.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hide-workflowy/hide_workflowyApp.swift
@@ -0,0 +1,17 @@
+//
+//  hide_workflowyApp.swift
+//  hide-workflowy
+//
+//  Created by Oleh Kopyl on 17.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct hide_workflowyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
